66797f5aafb63807da793c56e27a937d15e6ca83
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-03-16 15:42:47 +0100

    FormateurRepository

--- AppliCFABack/src/main/java/fr/dawan/AppliCFABack/repositories/FormateurRepository.java
+++ AppliCFABack/src/main/java/fr/dawan/AppliCFABack/repositories/FormateurRepository.java
@@ -1,8 +1,11 @@
 package fr.dawan.AppliCFABack.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import fr.dawan.AppliCFABack.entities.Formateur;
+
 @Repository
-public interface FormateurRepository {
+public interface FormateurRepository extends JpaRepository<Formateur, Long> {
 
 }
