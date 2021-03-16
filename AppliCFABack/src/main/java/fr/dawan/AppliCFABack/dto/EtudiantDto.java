9c761ac2acab2f34b226b0f1e4f4af3c696f866b
Tanguy <tbillon@dawan.fr>
2021-03-16 15:42:55 +0100

    crude etudiant repo/service/controller

--- AppliCFABack/src/main/java/fr/dawan/AppliCFABack/dto/EtudiantDto.java
+++ AppliCFABack/src/main/java/fr/dawan/AppliCFABack/dto/EtudiantDto.java
@@ -5,26 +5,50 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @SuppressWarnings("serial")
 @XmlRootElement(name = "etudiant")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class EtudiantDto implements Serializable {
-
+	
+	@XmlElement
 	private long id;
-
+	
+	@XmlElement
 	private PersonneDto personne;
-
+	
+	@XmlElement
 	private List<AbsenceDto> abscences;
-
+	
+	@XmlElement
 	private EntrepriseDto entreprise;
-
+	
+	@XmlElement
 	private List<NoteDto> notes;
-
+	
+	@XmlElement
 	private List<PromotionDto> promotions;
-
+	
+	@XmlElement
 	private List<GroupeDto> groupes;
+
+	public EtudiantDto() {
+		super();
+	}
+	
+	public EtudiantDto(long id, PersonneDto personne, List<AbsenceDto> abscences, EntrepriseDto entreprise,
+			List<NoteDto> notes, List<PromotionDto> promotions, List<GroupeDto> groupes) {
+		super();
+		this.id = id;
+		this.personne = personne;
+		this.abscences = abscences;
+		this.entreprise = entreprise;
+		this.notes = notes;
+		this.promotions = promotions;
+		this.groupes = groupes;
+	}
 
 	public long getId() {
 		return id;
