b81ffeabceaac6a10498dca66f1518f6db4a30c5
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-03-17 12:23:07 +0100

    resolution pb DtoTools

--- AppliCFABack/src/main/java/fr/dawan/AppliCFABack/dto/DtoTools.java
+++ AppliCFABack/src/main/java/fr/dawan/AppliCFABack/dto/DtoTools.java
@@ -1,19 +1,21 @@
 package fr.dawan.AppliCFABack.dto;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 
 public class DtoTools {
-	
-	private static ModelMapper myMapper = new ModelMapper(); 
+
+	private static ModelMapper myMapper = new ModelMapper();
+
 	public static <TSource, TDestination> TDestination convert(TSource obj, Class<TDestination> clazz) {
-		
+
 		// TODO ajout de config pour personnaliser le mapping dto<>entity
 		// myMapper.typeMap(Contact.class, ContactDto.class)
 		// .addMappings(mapper->{
 		// mapper.map(src->src.getName(), ContactDto::setName);
 		// mapper.map(src->src.getEmail(), ContactDto::setEmail);
 		// });
-
+		myMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		return myMapper.map(obj, clazz);
 	}
 }
