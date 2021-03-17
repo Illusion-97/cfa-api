281b68ccc289399f1c6c611a40d67fb85f1c3ab3
Ka-Bis <k.sydaphasavanh@gmail.com>
2021-03-17 09:35:42 +0100

    implementation methode de la classe FormateurService dans le controller

--- AppliCFABack/src/main/java/fr/dawan/AppliCFABack/controllers/FormateurController.java
+++ AppliCFABack/src/main/java/fr/dawan/AppliCFABack/controllers/FormateurController.java
@@ -1,10 +1,23 @@
 package fr.dawan.AppliCFABack.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.dawan.AppliCFABack.dto.FormateurDto;
+import fr.dawan.AppliCFABack.services.FormateurService;
 
 @RestController
 @RequestMapping("/AppliCFABack/formateurs")
 public class FormateurController {
+	@Autowired
+	private FormateurService formateurService;
 
+	@GetMapping(produces = { "application/json", "application/xml" })
+	public List<FormateurDto> getAll() {
+		return formateurService.getAll();
+	}
 }
