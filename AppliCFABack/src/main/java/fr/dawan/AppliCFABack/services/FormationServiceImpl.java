ada76a1fc3eff2a998f2f4545992a8d062035206
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-06-10 11:31:03 +0200

    Get cursus from formation

--- AppliCFABack/src/main/java/fr/dawan/AppliCFABack/services/FormationServiceImpl.java
+++ AppliCFABack/src/main/java/fr/dawan/AppliCFABack/services/FormationServiceImpl.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import fr.dawan.AppliCFABack.dto.CursusDto;
 import fr.dawan.AppliCFABack.dto.DtoTools;
 import fr.dawan.AppliCFABack.dto.FormationDto;
+import fr.dawan.AppliCFABack.entities.Cursus;
 import fr.dawan.AppliCFABack.entities.Formation;
 import fr.dawan.AppliCFABack.repositories.FormationRepository;
 
@@ -29,14 +31,26 @@ public class FormationServiceImpl implements FormationService {
 
 		List<FormationDto> lstDto = new ArrayList<FormationDto>();
 		for (Formation f : lst) {
-			lstDto.add(DtoTools.convert(f, FormationDto.class));
+			FormationDto formationDto = DtoTools.convert(f, FormationDto.class);
+			
+			List<Cursus> lstCursus = f.getCursusLst();
+			List<CursusDto> lstCursusDto = new ArrayList<CursusDto>();
+			
+			for (Cursus cursus : lstCursus) {
+				if (cursus != null)
+					lstCursusDto.add(DtoTools.convert(cursus, CursusDto.class));
+			}
+			
+			formationDto.setCursusLstDto(lstCursusDto);
+			lstDto.add(formationDto);
 		}
 		return lstDto;
 	}
 
 	@Override
 	public List<FormationDto> getAllFormation(int page, int size) {
-		List<Formation> lst = formationRepository.findAll(PageRequest.of(page, size)).get().collect(Collectors.toList());
+		List<Formation> lst = formationRepository.findAll(PageRequest.of(page, size)).get()
+				.collect(Collectors.toList());
 
 		// conversion vers Dto
 		List<FormationDto> lstDto = new ArrayList<FormationDto>();
@@ -58,9 +72,9 @@ public class FormationServiceImpl implements FormationService {
 	@Override
 	public FormationDto saveOrUpdate(FormationDto fDto) {
 		Formation f = DtoTools.convert(fDto, Formation.class);
-		
+
 		f = formationRepository.saveAndFlush(f);
-		
+
 		return DtoTools.convert(f, FormationDto.class);
 	}
 
