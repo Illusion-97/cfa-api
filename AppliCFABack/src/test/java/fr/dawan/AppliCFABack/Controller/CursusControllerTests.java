2dd39228945c430e4cd3212ff1bb956e3ab65edf
Nicolas <npotier@dawan.fr>
2021-09-23 09:42:01 +0200

    CRUD test Cursus

--- AppliCFABack/src/test/java/fr/dawan/AppliCFABack/Controller/CursusControllerTests.java
+++ AppliCFABack/src/test/java/fr/dawan/AppliCFABack/Controller/CursusControllerTests.java
@@ -1,41 +1,151 @@
 package fr.dawan.AppliCFABack.Controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import fr.dawan.AppliCFABack.controllers.CursusController;
+import fr.dawan.AppliCFABack.dto.CursusDto;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(Lifecycle.PER_CLASS)
 public class CursusControllerTests {
 
-//	@Autowired
-//	private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 	
 	@Autowired
-	private CursusController CursusController;
+	private CursusController cursusController;
+	
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private long idCursus;
 
 	
 	@Test
 	void contextLoads() {
-		assertThat(CursusController).isNotNull();
+		assertThat(cursusController).isNotNull();
 		
 	}
 	
-	void deleteDatabase() {
-		
+	@BeforeAll
+	void init() {
+		assertThat(cursusController).isNotNull();
+//		initDataBase();
 	}
 	
-	void initDataBase() {
-		
-	
+	@AfterAll
+	void clean(){
+//		testDelete();
+//		deleteDatabase();
+	}
+	@Test
+	void testFindAll() {
+		try {
+			mockMvc.perform(get("/AppliCFABack/cursus").accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk());
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
 	}
 	
 	@Test
-	void testFindAll() {
-//		
+	void testFindById() {
+		try {
+			mockMvc.perform(get("/AppliCFABack/cursus/" + idCursus).accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("$.titre", is("titre 1")));
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	void testSave() {
+		try {
+			CursusDto cToInsert = new CursusDto();
+			cToInsert.setTitre("titre cursus save");
+			
+
+			objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+			String jsonReq = objectMapper.writeValueAsString(cToInsert);
+
+			String jsonReponse = mockMvc.perform(post("/AppliCFABack/cursus")
+					.contentType(MediaType.APPLICATION_JSON) 
+					.accept(MediaType.APPLICATION_JSON)
+					.content(jsonReq)).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+			CursusDto cDto = objectMapper.readValue(jsonReponse, CursusDto.class);
+			assertTrue(cDto.getId() != 0);
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	void testUpdate() {
+
+		try {
+			CursusDto cDto = cursusController.getById(idCursus+1);
+			cDto.setTitre("titre cursus update");
+
+			objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+			String jsonReq = objectMapper.writeValueAsString(cDto);
+
+			String jsonReponse = mockMvc.perform(put("/AppliCFABack/cursus") 
+					.contentType(MediaType.APPLICATION_JSON) 
+					.accept(MediaType.APPLICATION_JSON) 
+					.content(jsonReq)).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+			CursusDto res = objectMapper.readValue(jsonReponse, CursusDto.class);
+			assertEquals(res.getId(), cDto.getId());
+			assertEquals(res.getTitre(), cDto.getTitre());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+
+	}
+	
+	@Test
+	void testDelete() {
+
+		try {
+			String rep = mockMvc.perform(delete("/AppliCFABack/cursus/"+ idCursus) 
+					.accept(MediaType.TEXT_PLAIN))
+					.andExpect(status().isAccepted()).andReturn().getResponse().getContentAsString();
+			assertEquals("suppression effectuée", rep);
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
 	}
 }
