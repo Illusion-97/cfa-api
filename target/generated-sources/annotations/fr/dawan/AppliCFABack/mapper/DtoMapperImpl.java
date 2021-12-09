ecbf70cb3b0931cc3c3f52281c7c40d1b8b53b7d
Kanha Sydaphasavanh <ksydaphasavanh@dawan.fr>
2021-12-09 20:09:22 +0100

    .class file

--- target/generated-sources/annotations/fr/dawan/AppliCFABack/mapper/DtoMapperImpl.java
+++ target/generated-sources/annotations/fr/dawan/AppliCFABack/mapper/DtoMapperImpl.java
@@ -62,8 +62,8 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-11-30T09:19:35+0100",
-    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 15.0.2 (Oracle Corporation)"
+    date = "2021-12-09T18:38:52+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_271 (Oracle Corporation)"
 )
 public class DtoMapperImpl implements DtoMapper {
 
