b8d374129b4327840573d932e36e39103176de27
Valentin Chevallereau <chevallereau.valentin@outlook.fr>
2022-06-22 09:24:36 +0200

    AppelDG2Promotions

--- src/main/java/fr/dawan/AppliCFABack/services/PromotionService.java
+++ src/main/java/fr/dawan/AppliCFABack/services/PromotionService.java
@@ -44,5 +44,5 @@ public interface PromotionService {
 	
 	int fetchDGPromotion(String email, String password) throws Exception;
 	
-	List<Promotion> getPromotionDG2ByIdCursus(String email, String password, long idCursis) throws Exception;
+	List<Promotion> getPromotionDG2ByIdCursusDG2(String email, String password, long idCursis) throws Exception;
 }
