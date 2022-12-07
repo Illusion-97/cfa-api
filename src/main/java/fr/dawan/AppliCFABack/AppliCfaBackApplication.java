9615f925ed71a020e8ace1a620e77f1971d5c22f
Nicolas Potier <npotier@dawan.fr>
2022-12-07 16:48:58 +0000

    Update AppliCfaBackApplication.java

--- src/main/java/fr/dawan/AppliCFABack/AppliCfaBackApplication.java
+++ src/main/java/fr/dawan/AppliCFABack/AppliCfaBackApplication.java
@@ -54,7 +54,7 @@ public class AppliCfaBackApplication {
 			// Intercepteurs
 			@Override
 			public void addInterceptors(InterceptorRegistry registry) {
-				// registry.addInterceptor(tokenInterceptor);
+				registry.addInterceptor(tokenInterceptor);
 			}
 
 			@Override
