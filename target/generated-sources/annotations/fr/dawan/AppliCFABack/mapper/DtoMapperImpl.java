d2844a827decfe69349be3c3a5ac5f15367d21db
Kirigaya-K <rithshy@gmail.com>
2023-02-15 15:38:29 +0100

    db cfa

--- target/generated-sources/annotations/fr/dawan/AppliCFABack/mapper/DtoMapperImpl.java
+++ target/generated-sources/annotations/fr/dawan/AppliCFABack/mapper/DtoMapperImpl.java
@@ -68,7 +68,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-02-14T11:54:09+0100",
+    date = "2023-02-15T15:27:58+0100",
     comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.300.v20221108-0856, environment: Java 17.0.5 (Oracle Corporation)"
 )
 public class DtoMapperImpl implements DtoMapper {
