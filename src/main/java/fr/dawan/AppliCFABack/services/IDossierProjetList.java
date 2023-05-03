5a3f69f03143a4bf3961554d0f8d012c354984bf
Chaymae Fayzi <cfayzi@dawan.fr>
2023-05-03 11:44:22 +0200

    uploadDossierPro

---
+++ src/main/java/fr/dawan/AppliCFABack/services/IDossierProjetList.java
@@ -0,0 +1,8 @@
+package fr.dawan.AppliCFABack.services;
+
+import fr.dawan.AppliCFABack.entities.DossierProjet;
+
+public interface IDossierProjetList {
+
+	void setDossierProjet(DossierProjet dp);
+}
