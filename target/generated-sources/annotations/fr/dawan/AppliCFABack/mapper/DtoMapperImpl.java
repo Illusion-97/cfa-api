8d8d714c3b82e4dd441fa91086be66c7bb365cac
Nicolas <npotier@dawan.fr>
2023-06-12 10:04:40 +0200

    Fetch etudiant et intervention ok

--- target/generated-sources/annotations/fr/dawan/AppliCFABack/mapper/DtoMapperImpl.java
+++ target/generated-sources/annotations/fr/dawan/AppliCFABack/mapper/DtoMapperImpl.java
@@ -83,7 +83,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-01T16:07:11+0200",
+    date = "2023-06-12T09:36:54+0200",
     comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 15.0.1 (Oracle Corporation)"
 )
 public class DtoMapperImpl implements DtoMapper {
