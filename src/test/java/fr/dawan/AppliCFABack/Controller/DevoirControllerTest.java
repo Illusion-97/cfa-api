67df52ccf15a96bbcf34fd4e0f145511dfc6a140
hugo Torres <htorres@dawan.fr>
2023-07-03 16:11:31 +0200

    modif format date

--- src/test/java/fr/dawan/AppliCFABack/Controller/DevoirControllerTest.java
+++ src/test/java/fr/dawan/AppliCFABack/Controller/DevoirControllerTest.java
@@ -1,9 +1,19 @@
 package fr.dawan.AppliCFABack.Controller;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.dawan.AppliCFABack.controllers.DevoirController;
-import fr.dawan.AppliCFABack.dto.DevoirDto;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,16 +25,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import fr.dawan.AppliCFABack.controllers.DevoirController;
+import fr.dawan.AppliCFABack.dto.DevoirDto;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -84,8 +89,8 @@ class DevoirControllerTest {
 		try {
 			DevoirDto eToInsert = new DevoirDto();
 			eToInsert.setConsigne("enonce save");
-			eToInsert.setDateDebut(LocalDateTime.now());
-			eToInsert.setDateFin(LocalDateTime.now());
+			eToInsert.setDateDebut(LocalDate.now());
+			eToInsert.setDateFin(LocalDate.now());
 
 			objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
 			String jsonReq = objectMapper.writeValueAsString(eToInsert);
@@ -109,8 +114,8 @@ class DevoirControllerTest {
 		try {
 			DevoirDto eDto = devoirController.findById(idDevoir +1);
 			eDto.setConsigne("enonce update");
-			eDto.setDateDebut(LocalDateTime.now());
-			eDto.setDateFin(LocalDateTime.now());
+			eDto.setDateDebut(LocalDate.now());
+			eDto.setDateFin(LocalDate.now());
 
 			objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
 			String jsonReq = objectMapper.writeValueAsString(eDto);
