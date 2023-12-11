debf3a20d626d5f5c5c7d7de707c4225c1c99420
Nicolas <nicolaspotier1@gmail.com>
2023-12-11 15:43:01 +0100

    inscription tuteur exception ok

--- src/main/java/fr/dawan/AppliCFABack/controllers/UtilisateurController.java
+++ src/main/java/fr/dawan/AppliCFABack/controllers/UtilisateurController.java
@@ -1,9 +1,13 @@
 package fr.dawan.AppliCFABack.controllers;
 
 import fr.dawan.AppliCFABack.dto.*;
+import fr.dawan.AppliCFABack.services.UtilisateurRoleServiceImpl;
 import fr.dawan.AppliCFABack.services.UtilisateurService;
 import fr.dawan.AppliCFABack.tools.SaveInvalidException;
 import javassist.NotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,222 +21,225 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/utilisateurs")
 public class UtilisateurController {
-	
-    @Autowired
-    private UtilisateurService utilisateurService;
-    
 
-    @DeleteMapping(value = "/{id}", produces = "text/plain")
-    public ResponseEntity<?> deleteById(@PathVariable(value = "id") long id) {
-        try {
-            utilisateurService.deleteById(id);
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body("suppression effectuée");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("suppression non réalisée");
-        }
+	@Autowired
+	private UtilisateurService utilisateurService;
 
-    }
+	private static final Logger logger = LoggerFactory.getLogger(UtilisateurController.class);
 
-    @GetMapping(value = "/{page}/{size}", produces = "application/json")
-    public @ResponseBody
-    List<UtilisateurDto> getAllByPage(
-            @PathVariable("page") int page,
-            @PathVariable(value = "size") int size,
-            @RequestParam(required = false) Optional<String> role,
-            @RequestParam(defaultValue = "", required = false) Optional<String> search) {
+	@DeleteMapping(value = "/{id}", produces = "text/plain")
+	public ResponseEntity<?> deleteById(@PathVariable(value = "id") long id) {
+		try {
+			utilisateurService.deleteById(id);
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body("suppression effectuée");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("suppression non réalisée");
+		}
 
-        if (role.isPresent() && search.isPresent())
-            return utilisateurService.findAllByRoleByPage(page, size, role.get(), search.get());
-        else
-            return utilisateurService.getAllUtilisateurs(page, size, search.get());
-    }
+	}
 
-    @GetMapping(value = "/count", produces = "application/json")
-    public CountDto count(@RequestParam(required = false) Optional<String> role,
-                          @RequestParam(defaultValue = "", required = false) Optional<String> search) {
-        if (role.isPresent() && search.isPresent())
-            return utilisateurService.countByRole(role.get(), search.get());
-        else
-            return utilisateurService.count(search.get());
-    }
-    
-    /**
-     * 
-     * @param id
-     * @return recuperation des user pat id
-     */
-    @GetMapping(value = "/{id}", produces = "application/json")
-    public UtilisateurDto getById(@PathVariable("id") long id) {
-        return utilisateurService.getById(id);
-    }
+	@GetMapping(value = "/{page}/{size}", produces = "application/json")
+	public @ResponseBody List<UtilisateurDto> getAllByPage(@PathVariable("page") int page,
+			@PathVariable(value = "size") int size, @RequestParam(required = false) Optional<String> role,
+			@RequestParam(defaultValue = "", required = false) Optional<String> search) {
 
-    /**
-     * 
-     * @return
-     */
-    @GetMapping(produces = {"application/json", "application/xml"}, value = "/with-object")
-    public List<UtilisateurDto> getAllWithObject() {
-        return utilisateurService.getAllWithObject();
-    }
+		if (role.isPresent() && search.isPresent())
+			return utilisateurService.findAllByRoleByPage(page, size, role.get(), search.get());
+		else
+			return utilisateurService.getAllUtilisateurs(page, size, search.get());
+	}
 
-    /**
-     * 
-     * @return recuperation de tous les users
-     */
-    @GetMapping(produces = {"application/json", "application/xml"})
-    public List<UtilisateurDto> getAll() {
-        return utilisateurService.getAll();
-    }
-    
-    /**
-     * 
-     * @param login
-     * @return recherche par email
-     */
-    @GetMapping(value = "/email={login}", produces = "application/json")
-    public UtilisateurDto getByLogin(@PathVariable("login") String login) {
-        return utilisateurService.findByEmail(login);
-    }
+	@GetMapping(value = "/count", produces = "application/json")
+	public CountDto count(@RequestParam(required = false) Optional<String> role,
+			@RequestParam(defaultValue = "", required = false) Optional<String> search) {
+		if (role.isPresent() && search.isPresent())
+			return utilisateurService.countByRole(role.get(), search.get());
+		else
+			return utilisateurService.count(search.get());
+	}
 
-    /**
-     * 
-     * @param id
-     * @return
-     */
-    @GetMapping(value = "/{id}/with-object", produces = {"application/json", "application/xml"})
-    public UtilisateurDto getByIdWithObject(@PathVariable("id") long id) {
-        return utilisateurService.getByIdWithObject(id);
-    }
+	/**
+	 * 
+	 * @param id
+	 * @return recuperation des user pat id
+	 */
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public UtilisateurDto getById(@PathVariable("id") long id) {
+		return utilisateurService.getById(id);
+	}
 
-    /**
-     * 
-     * @param name
-     * @return ResponseEntity
-     */
-    @GetMapping(value = "/user", produces = "application/json")
-    public ResponseEntity<?> getName(@RequestParam("name") String name) {
-        UtilisateurDto user = utilisateurService.getName(name);
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	@GetMapping(produces = { "application/json", "application/xml" }, value = "/with-object")
+	public List<UtilisateurDto> getAllWithObject() {
+		return utilisateurService.getAllWithObject();
+	}
 
-    /**
-     * 
-     * @param uDto
-     * @return ResponseEntity
-     */
-    @PostMapping(consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> insert(@RequestBody UtilisateurDto uDto) {
-        try {
-            return ResponseEntity.ok(utilisateurService.insertUpdate(uDto));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
-    }
-    
-    @PostMapping(value = "/tuteur", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> insertTuteur(@RequestBody UtilisateurDto uDto) throws SaveInvalidException {
-        try {
-            return ResponseEntity.ok(utilisateurService.insertTuteur(uDto));
-        } catch (Exception e) {
+	/**
+	 * 
+	 * @return recuperation de tous les users
+	 */
+	@GetMapping(produces = { "application/json", "application/xml" })
+	public List<UtilisateurDto> getAll() {
+		return utilisateurService.getAll();
+	}
+
+	/**
+	 * 
+	 * @param login
+	 * @return recherche par email
+	 */
+	@GetMapping(value = "/email={login}", produces = "application/json")
+	public UtilisateurDto getByLogin(@PathVariable("login") String login) {
+		return utilisateurService.findByEmail(login);
+	}
+
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@GetMapping(value = "/{id}/with-object", produces = { "application/json", "application/xml" })
+	public UtilisateurDto getByIdWithObject(@PathVariable("id") long id) {
+		return utilisateurService.getByIdWithObject(id);
+	}
+
+	/**
+	 * 
+	 * @param name
+	 * @return ResponseEntity
+	 */
+	@GetMapping(value = "/user", produces = "application/json")
+	public ResponseEntity<?> getName(@RequestParam("name") String name) {
+		UtilisateurDto user = utilisateurService.getName(name);
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+	/**
+	 * 
+	 * @param uDto
+	 * @return ResponseEntity
+	 */
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> insert(@RequestBody UtilisateurDto uDto) {
+		try {
+			return ResponseEntity.ok(utilisateurService.insertUpdate(uDto));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+
+	@PostMapping(value = "/tuteur", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> insertTuteur(@RequestBody UtilisateurDto uDto) {
+		try {
+			return ResponseEntity.ok(utilisateurService.insertTuteur(uDto));
+		} catch (SaveInvalidException e) {
+			// L'email existe déjà en base
+			logger.error("Un utilisateur utilise déjà cette adresse mail login", e);
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+		} catch (Exception e) {
+			logger.error("Une erreur s'est produite lors de l'insertion du tuteur.", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Une erreur est survenue lors de l'insertion du tuteur.");
+		}
+	}
+
+	@PutMapping(value = "/tuteur", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> updateTuteur(@RequestBody UtilisateurDto uDto) throws SaveInvalidException {
+		try {
+			return ResponseEntity.ok(utilisateurService.updateTuteur(uDto));
+		} catch (Exception e) {
 			throw new SaveInvalidException(e.getMessage());
-        }
-    }
-    
-    @PutMapping(value = "/tuteur", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> updateTuteur(@RequestBody UtilisateurDto uDto) throws SaveInvalidException {
-        try {
-            return ResponseEntity.ok(utilisateurService.updateTuteur(uDto));
-        } catch (Exception e) {
-			throw new SaveInvalidException(e.getMessage());
-        }
-    }
+		}
+	}
 
-    /**
-     * 
-     * @param uDto
-     * @return mise à jour des user
-     * @throws Exception
-     */
-    @PutMapping(consumes = "application/json", produces = "application/json")
-    public UtilisateurDto update(@RequestBody UtilisateurDto uDto) throws SaveInvalidException {
-        return utilisateurService.insertUpdate(uDto);
-    }
+	/**
+	 * 
+	 * @param uDto
+	 * @return mise à jour des user
+	 * @throws Exception
+	 */
+	@PutMapping(consumes = "application/json", produces = "application/json")
+	public UtilisateurDto update(@RequestBody UtilisateurDto uDto) throws SaveInvalidException {
+		return utilisateurService.insertUpdate(uDto);
+	}
 
-    /**
-     * 
-     * @param ville
-     * @return recjerche par adresse(ville)
-     */
-    @GetMapping(value = "/adresse", produces = "application/json")
-    public List<UtilisateurDto> findByAdresse(@RequestParam("ville") String ville) {
-        return utilisateurService.findByAdresse(ville);
-    }
+	/**
+	 * 
+	 * @param ville
+	 * @return recjerche par adresse(ville)
+	 */
+	@GetMapping(value = "/adresse", produces = "application/json")
+	public List<UtilisateurDto> findByAdresse(@RequestParam("ville") String ville) {
+		return utilisateurService.findByAdresse(ville);
+	}
 
 //	@GetMapping(value = "/entreprise", produces = "application/json")
 //	public List<UtilisateurDto> findByEntreprise(@RequestParam("id") long id) {
 //		return utilisateurService.findByEntreprise(id);
 //	}
 
-    @GetMapping(value = "/roles/{idRole}", produces = "application/json")
-    public List<UtilisateurDto> findByRole(@PathVariable("role") long idRole) {
-        return utilisateurService.findByRole(idRole);
-    }
+	@GetMapping(value = "/roles/{idRole}", produces = "application/json")
+	public List<UtilisateurDto> findByRole(@PathVariable("role") long idRole) {
+		return utilisateurService.findByRole(idRole);
+	}
 
+	@GetMapping(value = "/{id}/planning", produces = "application/json")
+	public List<JourneePlanningDto> getAllJourneePlanningByIdUtilisateur(@PathVariable("id") long id) {
+		return utilisateurService.getAllJourneePlanningByIdUtilisateur(id);
+	}
 
-    @GetMapping(value = "/{id}/planning", produces = "application/json")
-    public List<JourneePlanningDto> getAllJourneePlanningByIdUtilisateur(@PathVariable("id") long id) {
-        return utilisateurService.getAllJourneePlanningByIdUtilisateur(id);
-    }
+	@GetMapping(value = "/{id}/conges", produces = "application/json")
+	public List<CongeDto> getAllCongesByIdUtilisateur(@PathVariable("id") long id) {
+		return utilisateurService.getAllCongesByIdUtilisateur(id);
+	}
 
-    @GetMapping(value = "/{id}/conges", produces = "application/json")
-    public List<CongeDto> getAllCongesByIdUtilisateur(@PathVariable("id") long id) {
-        return utilisateurService.getAllCongesByIdUtilisateur(id);
-    }
+	/*
+	 * On récupère l'adresse de l'étudiant à partir de son id en passant par
+	 * Personne
+	 */
+	@GetMapping(value = "/{id}/adresse", produces = "application/json")
+	public AdresseDto getAdresseByIdUtilisateur(@PathVariable("id") long id) {
+		return utilisateurService.getAdresseByIdUtilisateur(id);
+	}
 
-    /*
-     * On récupère l'adresse de l'étudiant à partir de son id en passant par
-     * Personne
-     */
-    @GetMapping(value = "/{id}/adresse", produces = "application/json")
-    public AdresseDto getAdresseByIdUtilisateur(@PathVariable("id") long id) {
-        return utilisateurService.getAdresseByIdUtilisateur(id);
-    }
+	@GetMapping(value = "/{id}/isReferent", produces = "application/json")
+	public Boolean isReferent(@PathVariable("id") long id) {
+		return utilisateurService.isReferent(id);
+	}
 
-    @GetMapping(value = "/{id}/isReferent", produces = "application/json")
-    public Boolean isReferent(@PathVariable("id") long id) {
-        return utilisateurService.isReferent(id);
-    }
-
-    @PostMapping(value = "{id}/upload-file", consumes = "multipart/form-data")
-    public ResponseEntity<?> uploadFile(@PathVariable("id") long id, @RequestParam("file") MultipartFile file) {
-        try {
-            utilisateurService.uploadFile(file, id);
-            return ResponseEntity.status(HttpStatus.OK).body("Import du fichier réussi");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
-    }
-    @GetMapping(value = "/dg2", produces = "application/json")
-	public ResponseEntity<String> fetchAllDG2(@RequestHeader Map<String, String> headers) {
-			String userDG2 = headers.get("x-auth-token");
-			String[] splitUserDG2String = userDG2.split(":");
-
-			try {
-				utilisateurService.fetchAllDG2Employees(splitUserDG2String[0], splitUserDG2String[1]);
-				return ResponseEntity.status(HttpStatus.OK).body("Succeed to fetch data from the webservice DG2");
-			} catch (Exception e) {
-				e.printStackTrace();
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-						.body("Error while fetching data from the webservice DG2");
-			}
+	@PostMapping(value = "{id}/upload-file", consumes = "multipart/form-data")
+	public ResponseEntity<?> uploadFile(@PathVariable("id") long id, @RequestParam("file") MultipartFile file) {
+		try {
+			utilisateurService.uploadFile(file, id);
+			return ResponseEntity.status(HttpStatus.OK).body("Import du fichier réussi");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		}
-    
-    @PutMapping("/{utilisateurId}/roles")
-    public ResponseEntity<String> modifierRolesUtilisateur(@PathVariable long utilisateurId,
-                                                           @RequestBody List<Long> nouveauRolesIds) throws NotFoundException {
-        utilisateurService.modifierRolesUtilisateur(utilisateurId, nouveauRolesIds);
-        return ResponseEntity.ok("Rôles de l'utilisateur modifiés avec succès");
-    }
+	}
+
+	@GetMapping(value = "/dg2", produces = "application/json")
+	public ResponseEntity<String> fetchAllDG2(@RequestHeader Map<String, String> headers) {
+		String userDG2 = headers.get("x-auth-token");
+		String[] splitUserDG2String = userDG2.split(":");
+
+		try {
+			utilisateurService.fetchAllDG2Employees(splitUserDG2String[0], splitUserDG2String[1]);
+			return ResponseEntity.status(HttpStatus.OK).body("Succeed to fetch data from the webservice DG2");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Error while fetching data from the webservice DG2");
+		}
+	}
+
+	@PutMapping("/{utilisateurId}/roles")
+	public ResponseEntity<String> modifierRolesUtilisateur(@PathVariable long utilisateurId,
+			@RequestBody List<Long> nouveauRolesIds) throws NotFoundException {
+		utilisateurService.modifierRolesUtilisateur(utilisateurId, nouveauRolesIds);
+		return ResponseEntity.ok("Rôles de l'utilisateur modifiés avec succès");
+	}
 
 }
-
