cd8d0bf9eeb0e61ddb25a4e468e3e238ef889198
Romain BLANQUET <rblanquet@dawan.fr>
2024-01-17 10:30:54 +0100

    fix token interceptor not checking if expired + clean up code

--- src/main/java/fr/dawan/AppliCFABack/interceptors/TokenInterceptor.java
+++ src/main/java/fr/dawan/AppliCFABack/interceptors/TokenInterceptor.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -19,42 +22,55 @@ public class TokenInterceptor implements HandlerInterceptor {
 
 	private static final Logger logger = Logger.getGlobal();
 
+	private final List<String> PUBLIC_URIS = Arrays.asList(
+			"/authenticate",
+			"/",
+			"/error",
+			"/forgot",
+			"/utilisateurs/tuteur",
+			"/reset-password",
+			"/subscribe"
+	);
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		if (!request.getMethod().equals("OPTIONS")) {
-			if (!request.getRequestURI().equals("/authenticate")
-					&& !request.getRequestURI().equals("/")
-					&& !request.getRequestURI().equals("/error")
-					&& !request.getRequestURI().equals("/forgot")
-					&& !request.getRequestURI().equals("/utilisateurs/tuteur")
-					&& !request.getRequestURI().equals("/reset-password")) {
-				String headerAuth = request.getHeader("Authorization");
-				logger.info(">>>>>> inside Token Interceptor...");
-				logger.info("URI =" + request.getRequestURI());
-				logger.info("Header (authorization) :" + request.getHeader("Authorization"));
+		if(request.getMethod().equals("OPTIONS")) {
+			return true;
+		}
+		if(this.isPublicUri(request.getRequestURI())) {
+			return true;
+		}
+		String headerAuth = request.getHeader("Authorization");
+		logger.info(">>>>>> inside Token Interceptor...");
+		logger.info("URI =" + request.getRequestURI());
+		logger.info("Header (authorization) :" + headerAuth);
 
-				if (headerAuth == null || headerAuth.trim().isEmpty() || !headerAuth.startsWith("Bearer ")) {
-					throw new MalformedJwtException("Erreur : jeton absent ou invalide !");
-				}
+		if (headerAuth == null || headerAuth.trim().isEmpty() || !headerAuth.startsWith("Bearer ")) {
+			throw new MalformedJwtException("Erreur : jeton absent ou invalide !");
+		}
 
-				String token = headerAuth.substring(7);
-				try {
-					// validation du token
-					jwtTokenUtil.isTokenExpired(token);
+		String token = headerAuth.substring(7);
+		try {
+			if(jwtTokenUtil.isTokenExpired(token)) {
+				throw new MalformedJwtException("Erreur : Token expired");
+			}
+			// validation du token
 					/*String email = jwtTokenUtil.getUsernameFromToken(token);
 					if (!TokenSaver.tokensByEmail.containsKey(email) || !TokenSaver.tokensByEmail.get(email).equals(token)) {
 						throw new MalformedJwtException("Erreur : jeton non reconnu !");
 					}*/
+			// TODO: Autres extractions du jeton ou autres traitements
 
-					// TODO: Autres extractions du jeton ou autres traitements
-
-				} catch (MalformedJwtException e) {
-					throw new MalformedJwtException("Erreur : jeton malformé !");
-				}
-			}
+		} catch (MalformedJwtException e) {
+			throw new MalformedJwtException("Erreur : jeton malformé !");
 		}
+
+
 		return true;
+	}
+
+	private boolean isPublicUri(String uri) {
+		return this.PUBLIC_URIS.contains(uri);
 	}
 }
 
